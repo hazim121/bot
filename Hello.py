838e49e14e2f26ad1985c301b0a066c876471988
hazim121 <152160746+hazim121@users.noreply.github.com>
2023-12-05 15:43:50 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -101,16 +101,16 @@ applications.''')
 
     if nav == "Chatbot":
         if prompt := st.chat_input("Hi! How can i help you?"):
-        # Display user message in chat message container
-        st.chat_message("user").markdown(prompt)
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        response = model_bot(prompt)
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            st.markdown(response)
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
+            # Display user message in chat message container
+            st.chat_message("user").markdown(prompt)
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            response = model_bot(prompt)
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                st.markdown(response)
+            # Add assistant response to chat history
+            st.session_state.messages.append({"role": "assistant", "content": response})
        
 # Function to load original data (replace this with your actual function)
 def load_original_data():
