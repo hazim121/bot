5b2af75867f74e815538d030d59b854898f04785
hazim121 <152160746+hazim121@users.noreply.github.com>
2023-12-05 16:44:16 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -77,10 +77,12 @@ applications.''')
             st.write(f"Size: {uploaded_file.size} bytes")
             # Read file content
             if uploaded_file.type == "application/pdf":
+                with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                    temp_file.write(uploaded_file.read())
                 #doc = io.BytesIO(uploaded_file.read())
-                #doc = textract.process(pdf_bytes)
-                #fil=doc.decode('utf-8')
-                text = uploaded_file.read()
+                doc = textract.process(temp_file.name))
+                fil=doc.decode('utf-8')
+                text = fil.read()
                 tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
     
                 def count_tokens(text: str) -> int:
