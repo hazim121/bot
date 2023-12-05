327d07db475bf1fcd099eee7383e520db77d4e04
hazim121 <152160746+hazim121@users.noreply.github.com>
2023-12-05 16:01:47 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -65,12 +65,30 @@ applications.''')
     #st.write(f"""<div style="overflow: hidden; overflow-wrap: break-word; height: 200px;">{long_text2}</div>""", unsafe_allow_html=True)
     if nav == "Upload PDF/DOC":
         
-        uploaded_file = st.file_uploader("Choose a file", type=["pdf", "doc"])
-        #doc = textract.process(uploaded_file)
-        with open(uploaded_file, 'w') as f:
-            f.write(doc.decode('utf-8'))
-        with open(uploaded_file, 'r') as f:
-            text = f.read()
+        
+        uploaded_file = st.file_uploader("Choose a file", type=["pdf"])
+        if uploaded_file is not None:
+            
+            st.write("File Details:")
+            st.write(f"Name: {uploaded_file.name}")
+            st.write(f"Type: {uploaded_file.type}")
+            st.write(f"Size: {uploaded_file.size} bytes")
+            # Read file content
+            if uploaded_file.type == "pdf":
+                
+				doc = textract.process(uploaded_file)
+				with open(doc, 'w') as f:
+				    f.write(doc.decode('utf-8'))
+				with open(doc, 'r') as f:
+					text = f.read()
+            else:
+                
+                st.warning("Unsupported file type. Please upload a pdf file.")
+        else:
+            
+            st.warning("Please upload a file.")
+        
+        
         
         tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
     
